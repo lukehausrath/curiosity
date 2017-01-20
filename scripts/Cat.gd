0fa0f62b720364d0ec914b621e221062111bcba9
Lucas <lhausrath@gmail.com>
2017-01-20 04:33:12 -0800

    Cat motion and misc changes
    Added cat motion and adjusted viewport properly

---
+++ scripts/Cat.gd
@@ -0,0 +1,30 @@
+# Script for the main character cat
+extends KinematicBody2D
+
+const WALK_SPEED = 50
+
+var velocity = Vector2()
+var animSprite = null
+
+
+func _fixed_process(delta):
+	if (Input.is_action_pressed("ui_left")):
+		velocity.x = -WALK_SPEED
+		animSprite.play("default")
+		animSprite.set_flip_h(true)
+	elif (Input.is_action_pressed("ui_right")):
+		velocity.x = WALK_SPEED
+		animSprite.play("default")
+		animSprite.set_flip_h(false)
+	else:
+		velocity.x = 0
+		animSprite.stop()
+	
+	var motion = velocity * delta
+	move(motion)
+
+
+func _ready():
+	set_fixed_process(true)
+	animSprite = get_node("AnimSprite")
+	assert(animSprite != null)
--- scripts/grass.gd
+++
@@ -1,7 +0,0 @@
-extends Sprite
-
-export var show_grass = false
-
-func _ready():
-	if (!show_grass):
-		get_node("grass").hide()
